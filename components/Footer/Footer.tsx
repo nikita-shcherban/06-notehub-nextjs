a25ad4e4cfede8a0dd02730947a980c959ac4ef9
NikitaShcherban <cruizsports@gmail.com>
2026-08-23 21:23:26 +0300

    Fix link tag for mailto, fix props layout

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
           <p>Developer: your name</p>
           <p>
             Contact us:
-            <Link href="<mailto:student@notehub.app>">student@notehub.app</Link>
+            <a href="mailto:student@notehub.app">student@notehub.app</a>
           </p>
         </div>
       </div>
